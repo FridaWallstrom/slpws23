444c5332e729a8c440c945af75fc8caf99b149cd
FridaWallstrom <93259332+FridaWallstrom@users.noreply.github.com>
2023-02-07 11:29:06 +0100

    lektion v5

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ before do
     cache_control :no_store, :max_age => 0 #för routes
     @user_id = session[:id]
     @logged_in = session[:login] 
-    @post_user_id = session[:post_user_id]
+    @username = session[:username]
 end 
 
 get('/login') do 
@@ -27,13 +27,18 @@ end
 post('/login') do 
     username = params[:username]
     password = params[:password]
+    p username 
+    p password 
     @db.results_as_hash = true 
     result = @db.execute("SELECT * FROM users WHERE username = ?", username).first
-    if !result 
+    if result == nil
         slim(:"error")
     end 
+    p result 
+    p BCrypt::Password.new(result["password"])
     if BCrypt::Password.new(result["password"]) == password
         session[:id] = Integer(result["id"])
+        session[:username] = result["username"]
         session[:login] = true 
         redirect('/')
     else 
@@ -51,22 +56,12 @@ get('/register') do
     slim(:"users/register")
 end 
 
-
-#fixa så att det inte är dublett (my_profile, profile)
-get('/my_profile') do 
+get('/user/:id/profile') do 
     @db.results_as_hash = true
-    result = @db.execute("SELECT * FROM users WHERE id = ?", @user_id).first
+    id = Integer(params[:id])
+    result = @db.execute("SELECT * FROM users WHERE id = ?", id).first
     username = result["username"]
-    you = true
-    slim(:"users/profile", locals: {username: username, you: you})
-end 
-
-get('/profile') do 
-    @db.results_as_hash = true
-    result = @db.execute("SELECT * FROM users WHERE id = ?", @post_user_id).first
-    username = result["username"]
-    you = false 
-    slim(:"users/profile", locals: {username: username, you: you})
+    slim(:"users/profile", locals: {username: username, id: id, user_id: @user_id})
 end 
 
 post('/register') do 
@@ -91,23 +86,26 @@ get('/') do
     @db.results_as_hash = true 
     result = @db.execute("SELECT * FROM posts ORDER BY id DESC")
     p "this is logged in: #{@logged_in}"
-    slim(:"posts/index", locals: {posts: result, logged_in: @logged_in})
+    slim(:"posts/index", locals: {posts: result, logged_in: @logged_in, user_id: @user_id})
 end 
 
-get('/user_post') do 
+get('/posts/:id/user_post') do 
     @db.results_as_hash = true 
-    result = @db.execute("SELECT * FROM posts WHERE user_id = ? ORDER BY id DESC", @user_id)
-    you = true 
-    slim(:"posts/user_post", locals: {posts: result, you: you})
+    id = Integer(params[:id])
+    result = @db.execute("SELECT * FROM posts WHERE user_id = ? ORDER BY id DESC", id)
+    if id != @user_id  
+        name = @db.execute("SELECT * FROM users WHERE id = ?", id).first
+        username = name["username"]
+    end 
+    slim(:"posts/user_post", locals: {posts: result, username: username, user_id: @user_id, id: id})
 end 
 
-get('/other_user_post') do 
+get('/posts/:id/user_comments') do 
     @db.results_as_hash = true 
-    result = @db.execute("SELECT * FROM posts WHERE user_id = ? ORDER BY id DESC", @post_user_id)
-    name = @db.execute("SELECT * FROM users WHERE id = ?",  @post_user_id).first
-    username = name["username"]
-    you = false 
-    slim(:"posts/user_post", locals: {posts: result, username: username, you: you})
+    id = Integer(params[:id])
+    result = @db.execute("SELECT * FROM comments WHERE user_id = ? ORDER BY id DESC", id)
+    p "this is result: #{result}"
+    slim(:"posts/user_comments", locals: {comments: result})
 end 
 
 get('/posts/new') do 
@@ -129,40 +127,22 @@ end
 
 ###
 
-get('/saved_post') do 
-    @db.results_as_hash = true 
-    result = @db.execute("SELECT posts.id, users.id FROM posts INNER JOIN users ON posts.id = users.id")
-    #blir detta ett nytt table som jag kan ta ifrån? 
-    slim(:"posts/saved_post", locals: {posts: result})
-end 
-
-post('/posts/:id/save') do
-    id = params[:id]
-    result = @db.execute("SELECT posts.id, users.id FROM posts INNER JOIN users ON posts.id = users.id WHERE id = ?", @user_id)
-    @db.execute("")
-end 
-
-post('/posts/category') do 
-#varje post ska ha en kategori, ska kunna sortera efter kategori 
-end 
-
-get('/comments/new') do 
-    slim(:"posts/show", locals: )
-end 
-
-post('/comments/new') do 
-
-end 
-
-#get('/posts/new') do 
-#    slim(:"posts/new", locals: {id: @user_id})
+#get('/saved_post') do 
+#    @db.results_as_hash = true 
+#    result = @db.execute("SELECT posts.id, users.id FROM posts INNER JOIN users ON posts.id = users.id")
+#    #blir detta ett nytt table som jag kan ta ifrån? 
+#    slim(:"posts/saved_post", locals: {posts: result})
 #end 
+#
+#post('/posts/:id/save') do
+#    id = params[:id]
+#    result = @db.execute("SELECT posts.id, users.id FROM posts INNER JOIN users ON posts.id = users.id WHERE id = ?", @user_id)
+#    @db.execute("")
+#end 
+#
 
-#post('/posts/new') do 
-#    content = params[:content]
-#    header = params[:header]
-#    @db.execute("INSERT INTO posts (header, content, user_id) VALUES (?,?,?)", header, content, @user_id)
-#    redirect('/')
+#post('/posts/category') do 
+##varje post ska ha en kategori, ska kunna sortera efter kategori 
 #end 
 
 ###
@@ -191,12 +171,22 @@ get('/posts/:id/show') do
     else 
         user_type = Integer(ut["user_type"])
     end 
-
     result = @db.execute("SELECT * FROM posts WHERE id = ?", id).first
-    session[:post_user_id] = Integer(result["user_id"])
-    @post_user_id = session[:post_user_id]
-    name = @db.execute("SELECT * FROM users WHERE id = ?", @post_user_id).first
+    post_user_id = Integer(result["user_id"])
+    name = @db.execute("SELECT * FROM users WHERE id = ?", post_user_id).first
     username = name["username"]
-    slim(:"posts/show", locals: {result: result, user_id: @user_id, post_user_id: @post_user_id, user_type: user_type, logged_in: @logged_in, username: username})
+    post_id = @db.execute("SELECT * FROM posts WHERE user_id = ?", post_user_id).first
+    comments = @db.execute("SELECT * FROM comments WHERE post_id = ? ORDER BY id DESC", id) 
+    slim(:"posts/show", locals: {result: result, user_id: @user_id, post_user_id: post_user_id, user_type: user_type, logged_in: @logged_in, username: username, comments: comments, my_username: @username})
 end 
 
+post('/posts/:post_id/comments/new') do 
+    post_id = params[:post_id]
+    content = params[:content]
+    @db.execute("INSERT INTO comments (content, user_id, post_id, username) VALUES (?,?,?,?)", content, @user_id, post_id, @username)
+    redirect("/posts/#{post_id}/show")
+end 
+
+#lägg till så man kan se vem som postat 
+#kategori, vän, spara posts, 
+#ändra, ta bort kommentar 
